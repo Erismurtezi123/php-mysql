49c6c12f523c0ff9bf12511ad60f9f146643421c
Erismurtezi123 <erismurtezi77@gmail.com>
2025-02-08 07:55:36 -0800

    //

---
+++ Day 3/function.php
@@ -0,0 +1,26 @@
+<?php
+
+function helloWorld(){
+    echo "hello world";
+}
+helloWorld();
+
+functiom sum(){
+    $value = 120 + 20;
+    echo $value;
+}
+
+function maximum ($sx, $y){
+    if($sx>$y){
+        return $x;
+    }else{
+        return $y;
+   }
+}
+
+$x-10;
+$y=20;
+
+$test = maximum($a,$b);
+echo"the bigger number was $test";
+?>
---
+++ Day 3/variables.php
@@ -0,0 +1,15 @@
+<?php
+
+$x -10;
+
+function variables(){
+    $y - 20;
+    global $x;
+    echo $y;
+
+}
+
+variables();
+echo">br>  $x"
+
+?>
