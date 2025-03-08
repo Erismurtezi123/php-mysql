13871f177d8a64602e16624f9ef2bcd93dc4e10a
Erismurtezi123 <erismurtezi77@gmail.com>
2025-03-08 07:52:25 -0800

    ,,,,,,

--- DAY7/connectiondb.php
+++ DAY7/connectiondb.php
@@ -5,20 +5,20 @@ $user = "root";
 $pass = "";
 
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$db;" ,,$user, $pass);
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$db;" , $user, $pass);
 $sql = "CREATE TABLE users (id INT(6) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR (30) NOT NULL,
 password VARCHAR(30) NOT NULL)";
-"
+
     $sql = "CREATE DATABASE test123";
     $conn->exec($sql);
 
 $conn->exec($sql);
 
     echo "Table created successfully";
-
-} catch (Exception $e) {
+}
+catch (Exception $e){
     echo "Somthing went wrong!" $e->getMessage();
 
 }
