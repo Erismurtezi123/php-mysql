7fea9b0eb55a03b633c01ef77d89e80fffc0de9f
Erismurtezi123 <erismurtezi77@gmail.com>
2025-03-22 16:16:49 +0100

    ......

--- DAY8/connection.php
+++ DAY8/connection.php
@@ -1,14 +1,22 @@
 <?php
 try{
-$pdo = new PDO("mysql:host=localhost;dbname=database"),
+$pdo = new PDO("mysql:host=localhost;dbname=erisss", "root", "");
 
-$username = "Eris";
-$password =password_hash("mypassword", PASSWORD_DEFAULT);
-$SQL = "INSERT INTO users(username,password) VALUES ("$username","$password")";
+//$username = "Erisss";
+//$password =password_hash("mypassword1234", PASSWORD_DEFAULT);
+//$email = "test@gmail.com";
+//$sql = "INSERT INTO users(username,password) VALUES ('$username','$password')";
+
+
+//$sql= "ALTER table users ADD email VARCHAR(255)";
+
+//$sql = "ALTER tables users DROP COLUMN email";
+
+$sql = "DROP TABLE PRODUCTS";
 
 $pdo->exec($sql);
 
-echo "New record create succesfully";
+echo "New Table deleted succesfully";
 }catch(DOExecption $e){
 
 {echo $e->getMessage();
