b8122d6ae3ccedf63acd5c12985a7c2dd37b168e
Erismurtezi123 <erismurtezi77@gmail.com>
2025-04-05 14:54:34 +0000

    fetch data

--- DAY9/add.php
+++ DAY9/add.php
@@ -1,23 +1,30 @@
 <?php
 include_once("config.php");
 
-if(isset($_POST['submit'])){
 
+if(isset($_POST['submit'])){
     $name = $_POST['name'];
     $username = $_POST['username'];
     $email = $_POST['email'];
 
-    $sql = "INSERT INTO users(name,username,email) VALUES (:name, :username,:email";
 
-    $sqlQuery ->prepare($sql);
-    $sqlQuery ->bindPrama (':name',$name);
-    $sqlQuery->bindPrama(':username',$username);
-    $sqlQuery->bindPrama(':email',$email);
+    $sql = "INSERT INTO users (name,username,email) VALUES (:name, :username, :email)";
+
+
+    $sqlQuery = $connect->prepare($sql);
+    $sqlQuery->bindParam(':name', $name);
+    $sqlQuery->bindParam(':username', $username);
+    $sqlQuery->bindParam(':email',$email);
+
 
     $sqlQuery->execute();
 
-    echo "The users was added succesfully";
+
+    echo "The user was added succesfully";
+
+
 }
+
 
 
 ?>
--- DAY9/config.php
+++ DAY9/config.php
@@ -1,13 +1,16 @@
 <?php
-$server = 'localhost';
-$username = 'root';
-$password = '';
-$dbname = 'users';
+ $server = 'localhost';
+ $username = 'root';
+ $password = '';
+ $dbname = 'db';
 
-try{
-    $content = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
-    }catch(Exception $e){
 
-    }
+ try{
+     $connect = new PDO("mysql:host=$server;dbname=$dbname", $username, $password);
+
+
+ }catch(Exeption $e){
+     echo "Something went wrong";
+ }
 ?>
 
